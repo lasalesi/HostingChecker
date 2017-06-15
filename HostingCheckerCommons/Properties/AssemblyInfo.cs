e04923a5980c6b6f3c1e6bcfe32e8ef0033c440f
lars.mueller <lars.mueller@sunrise.ch>
2017-06-15 11:18:40 +0200

    Initial app framework

---
+++ HostingCheckerCommons/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HostingCheckerCommons")]
+[assembly: AssemblyDescription("Common functions for hosting checker")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("libracore GmbH")]
+[assembly: AssemblyProduct("HostingCheckerCommons")]
+[assembly: AssemblyCopyright("Copyright © libracore GmbH, 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ca27242d-e8db-480f-aa20-a02564bbc3e3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
